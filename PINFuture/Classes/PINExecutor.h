e7c56ba1b1e6def9f726f0c953a939d4b9377764
Chris Danford <chris@pinterest.com>
2017-01-10 15:18:35 -0800

    update PINExecutor comments

--- PINFuture/Classes/PINExecutor.h
+++ PINFuture/Classes/PINExecutor.h
@@ -23,6 +23,9 @@ NS_ASSUME_NONNULL_BEGIN
 
 /**
  * Executes immediately on whatever the current thread is without trampolining.
+ * Use this as an optimization only if your block will execute extremely quickly, has no thread affinity
+ * (because it could be executed from anywhere) and the cost of executing the block is low compared to
+ * the cost of dispatching to a queue.
  */
 + (id<PINExecutor>)immediate;
 
@@ -43,7 +46,8 @@ NS_ASSUME_NONNULL_BEGIN
 
 /**
  * Executes immediately if the executor is invoked from the Main thread.  If the executor is not invoked from the Main thread
- * then uses `mainQueue`.
+ * then uses `mainQueue`.  Use this instead of `[PINExecutor mainQueue]` only if you know what you're doing and are looking to
+ * reduce the number of dispatches to the Main queue.
  */
 + (id<PINExecutor>)immediateOnMain;
 
